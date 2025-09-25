af6e2f61422ae6fdc49009e49778990e31949a38
Raushaniya12 <rausaniahusainova724@gmail.com>
2025-09-25 18:10:45 +0500

    .

---
+++ Lab_Rab_1_2_Husainova R.Z._BPI_23_02/Derivative.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Rab_1_2_Husainova_R.Z._BPI_23_02
+{
+    internal class Derivative:Function
+    {
+    }
+}
